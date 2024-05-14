d451c5bb7f7deccee38477e0015b1f4eb1fd445c
Dan Vega <danvega@gmail.com>
2024-05-14 08:16:21 -0400

    Cleaning up simple prompt example

--- src/main/java/dev/danvega/prompt/SimplePrompt.java
+++ src/main/java/dev/danvega/prompt/SimplePrompt.java
@@ -2,12 +2,8 @@ package dev.danvega.prompt;
 
 import org.springframework.ai.chat.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.time.LocalDateTime;
 
 @RestController
 public class SimplePrompt {
