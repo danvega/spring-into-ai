fa96f8fa6b649eb0ad4053740408ad4c999e6399
Dan Vega <danvega@gmail.com>
2024-05-14 08:15:13 -0400

    Adding weather config props for function calling

--- src/main/java/dev/danvega/functions/WeatherConfigProperties.java
+++ src/main/java/dev/danvega/functions/WeatherConfigProperties.java
@@ -4,4 +4,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(value = "weather")
 public record WeatherConfigProperties(String apiKey, String apiUrl) {
+
 }
