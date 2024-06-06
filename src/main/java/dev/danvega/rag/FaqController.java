2dab076f45b598c3c3c6c664a4b178790fa6b57e
Dan Vega <danvega@gmail.com>
2024-06-05 21:42:23 -0400

    Simplifying RAG demo to use Advisors

--- src/main/java/dev/danvega/rag/FaqController.java
+++ src/main/java/dev/danvega/rag/FaqController.java
@@ -1,41 +1,28 @@
 package dev.danvega.rag;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.document.Document;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 public class FaqController {
 
     private final ChatClient chatClient;
-    private final VectorStore vectorStore;
-    @Value("classpath:/prompts/rag-prompt-template.st")
-    private Resource ragPromptTemplate;
 
     public FaqController(ChatClient.Builder builder, VectorStore vectorStore) {
-        this.chatClient = builder.build();
-        this.vectorStore = vectorStore;
+        this.chatClient = builder
+                .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore,SearchRequest.defaults()))
+                .build();
     }
 
     @GetMapping("/faq")
     public String faq(@RequestParam(value = "message", defaultValue = "How many athletes compete in the Olympic Games Paris 2024") String message) {
-        List<Document> similarDocuments = vectorStore.similaritySearch(SearchRequest.query(message).withTopK(2));
-        List<String> contentList = similarDocuments.stream().map(Document::getContent).toList();
-
         return chatClient.prompt()
-                .user(u -> {
-                        u.text(ragPromptTemplate);
-                        u.param("input", message);
-                        u.param("documents", String.join("\n", contentList));
-                })
+                .user(message)
                 .call()
                 .content();
     }
